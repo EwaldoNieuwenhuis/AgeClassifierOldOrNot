8735c6658cf0229fd618efe455331dc7b8796766
Ewaldo Nieuwenhuis <s1171630@student.windesheim.nl>
2023-10-29 19:45:12 +0100

    Security updates

--- app.py
+++ app.py
@@ -4,14 +4,9 @@ Created on Thu Oct 26 12:25:24 2023
 
 @author: Gebruiker
 """
-import numpy as np
 import os
 from flask import Flask, render_template, request
 from tensorflow import keras
-from keras.preprocessing.image import load_img
-from keras.preprocessing.image import img_to_array
-from keras.applications.vgg16 import preprocess_input
-from keras.applications.vgg16 import decode_predictions
 from tensorflow.keras.preprocessing import image
 from werkzeug.utils import secure_filename
 
@@ -21,50 +16,55 @@ app = Flask(__name__)
 
 app.config["IMAGE_UPLOADS"] = "/Users/Gebruiker/Documents/Flask/static/images/"
 
-@app.route('/', methods = ['GET'])
+# Toegestane bestandsextensies
+ALLOWED_EXTENSIONS = {'jpg', 'jpeg', 'png'}
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/', methods=['GET'])
 def hello_world():
     return render_template('index.html')
 
-@app.route('/', methods = ['POST'])
+@app.route('/', methods=['POST'])
 def predict_age():
-    
-    # Specify the folder path where you want to delete files
+    # Folder voor het verwijderen van bestanden
     folder_path = "static/images/"
     
-    # List all files in the folder
+    # Lijst van bestanden in de map
     file_list = os.listdir(folder_path)
     
-    # Iterate through the list of files and delete each one
+    # Loop door de lijst van bestanden en verwijder elk bestand
     for filename in file_list:
         file_path = os.path.join(folder_path, filename)
         if os.path.isfile(file_path):
             os.remove(file_path)
-            
-            
+    
     imagefile = request.files['imagefile']
-    image_path =  imagefile.filename
-    filename = (image_path)
-    image_path= 'static/images/'+ imagefile.filename
-    imagefile.save(image_path)
-    #basedir = os.path.abspath(os.path.dirname(__file__))
-    #imagefile.save(os.path.join(basedir, app.config["IMAGE_UPLOADS"]))
     
-    img = image.load_img(image_path, target_size=(64, 64))  # Zorg ervoor dat je de juiste breedte en hoogte gebruikt
-    
-    # Converteer de afbeelding naar een numpy-array
-    img_array = image.img_to_array(img)
-    
-    # Voorverwerking van de afbeelding, zoals normalisatie
-    img_array = img_array / 255.0  # Normaliseer de pixelwaarden
-    
-    # Voorspelling maken met het getrainde model
-    prediction = model.predict(np.expand_dims(img_array, axis=0))  # Voeg een extra dimensie toe voor de batch
-    age = round(prediction[0][0])
-    old = 0
-    if(age > 42):
-        old = 1
-    
-    return render_template('index.html',  prediction=age, filename=filename, old = old)
+    # Controleer of het bestand is toegestaan
+    if imagefile and allowed_file(imagefile.filename):
+        image_path = os.path.join(app.config['IMAGE_UPLOADS'], secure_filename(imagefile.filename))
+        imagefile.save(image_path)
+        
+        img = image.load_img(image_path, target_size=(64, 64))  # Zorg ervoor dat je de juiste breedte en hoogte gebruikt
+
+        # Converteer de afbeelding naar een numpy-array
+        img_array = image.img_to_array(img)
+
+        # Voorverwerking van de afbeelding, zoals normalisatie
+        img_array = img_array / 255.0  # Normaliseer de pixelwaarden
+
+        # Voorspelling maken met het getrainde model
+        prediction = model.predict(np.expand_dims(img_array, axis=0))  # Voeg een extra dimensie toe voor de batch
+        age = round(prediction[0][0])
+        old = 0
+        if age > 42:
+            old = 1
+
+        return render_template('index.html', prediction=age, filename=secure_filename(imagefile.filename), old=old)
+    else:
+        return "Ongeldig bestandsformaat. Alleen JPEG en PNG zijn toegestaan."
 
 if __name__ == "__main__":
     app.run(port=3030, debug=True)
